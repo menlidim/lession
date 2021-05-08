2a253c90b34e2e35e23da433a08e113d632cebb0
Another acc <menliiakhmatovdg@lad24.ru>
2021-05-08 12:58:05 +0300

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let a = 12;
 let b = 'sdfdsfds';
-let c = 15;
+let c = 25;
 let d = 16;
 function getValue() {
   return a;
 }
 //sdsd commentsdsd
+function setValue() {
+  return a;
+}
+function setrValue() {
+  return a;
+}
